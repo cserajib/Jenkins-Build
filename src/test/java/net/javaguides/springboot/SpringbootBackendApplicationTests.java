b357e4b2705d7a38e35bcafaccda03ac5c54420b
cserajib <52009433+cserajib@users.noreply.github.com>
2023-02-10 03:19:17 +0530

    Update SpringbootBackendApplicationTests.java

--- src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
+++ src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
@@ -1,12 +1,11 @@
-/*
- * package net.javaguides.springboot;
- * 
- * import org.junit.jupiter.api.Test; import
- * org.springframework.boot.test.context.SpringBootTest;
- * 
- * @SpringBootTest class SpringbootBackendApplicationTests {
- * 
- * @Test void contextLoads() { }
- * 
- * }
- */
+  package net.javaguides.springboot;
+  
+  import org.junit.jupiter.api.Test; import
+  org.springframework.boot.test.context.SpringBootTest;
+  
+  @SpringBootTest class SpringbootBackendApplicationTests {
+  
+  @Test void contextLoads() { }
+  
+  }
+ 
